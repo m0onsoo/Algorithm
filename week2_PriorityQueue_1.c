cdb977ec03cdf0dcb26686dd3863890bbe0934a0
m0onsoo <63248777+m0onsoo@users.noreply.github.com>
2022-09-05 21:47:31 +0900

    Create week2_PriorityQueue_1.c

---
+++ week2_PriorityQueue_1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#pragma warning(disable:4996)
+
+int *findMax(int *x, int last) {
+	int *p, *max = x;
+	for (p = x; p < x + last; p++)
+		if (*p > *max)
+			max = p;
+	return max;
+}
+
+void swap(int *max, int *last) {
+	int tmp = *max;
+	*max = *last;
+	*last = tmp;
+}
+
+int main() {
+	int n, *x, *p, *max, *q, cnt=0, tmp, i;
+
+	scanf("%d", &n);
+	x = (int *)malloc(sizeof(int) * n);
+
+	for (p = x; p < x + n; p++)
+		scanf("%d", p);
+	max = x;
+	q = x;
+	for (i = 0; i < n; i++) {
+		max = findMax(x, n - cnt);
+		swap(max, x + n - cnt - 1);
+		cnt++;
+	}
+	for (p = x; p < x + n; p++)
+		printf("%d ", *p);
+
+	return 0;
+
+}
+
